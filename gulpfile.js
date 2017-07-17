ac5b3d5f6e28cacea10e208156ce2349f53b860d
Andre Marcelo-Tanner <andre@enthropia.com>
2017-07-17 22:28:20 +0800

    Adding gulp haml task

--- gulpfile.js
+++ gulpfile.js
@@ -8,17 +8,32 @@ var rename = require('gulp-rename');
 var less = require('gulp-less');
 var replace = require('gulp-replace');
 var merge = require('merge-stream');
+var haml = require('gulp-haml');
 
 // set paths
 var path = {
-   'src': './src',
-   'build': './build',
-   'nodemodules': './node_modules'
+  'src': {
+    'app': './src/app',
+    'base': './src',
+    'coffee': './src/coffee',
+    'haml': './src/haml',
+    'less': './src/less'
+  },
+  'build': {
+    'app': './build/app',
+    'base': './build',
+    'css': './build/assets/style',
+    'fonts': './build/assets/fonts',
+    'images': './build/assets/images',
+    'vendor': './build/assets/vendor'
+  },
+  'nodemodules': './node_modules'
 };
 
 // set of tasks to be run
 var tasks = [
-   'less'
+   'less',
+   'haml'
 ];
 
 gulp.task('less', function() {
@@ -32,7 +47,7 @@ gulp.task('less', function() {
   var streams = [];
 
   Object.keys(sources).forEach(function(sourceFileName) {
-    var src = path.src + '/less/' + sourceFileName;
+    var src = path.src.less + '/' + sourceFileName;
     var destFileName = sources[sourceFileName];
     var stream = gulp.src(src, {
          read: true
@@ -42,7 +57,30 @@ gulp.task('less', function() {
       .pipe(sourcemaps.write('.'))
       .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
       .pipe(rename(destFileName))
-      .pipe(gulp.dest(path.build + '/assets/style')
+      .pipe(gulp.dest(path.build.css)
+    );
+    streams.push(stream);
+  });
+
+  return merge(streams);
+});
+
+gulp.task('haml', function() {
+  var sources = {
+    'xml.haml': 'xml/index.html',
+    'contributing.haml': 'contributing/index.html'
+  };
+  var streams = [];
+
+  Object.keys(sources).forEach(function(sourceFileName) {
+    var src = path.src.haml + '/' + sourceFileName;
+    var destFileName = sources[sourceFileName];
+    var stream = gulp.src(src, {
+         read: true
+      })
+      .pipe(haml())
+      .pipe(rename(destFileName))
+      .pipe(gulp.dest(path.build.base)
     );
     streams.push(stream);
   });
